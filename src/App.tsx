47de7433c413daccc6576f8856bb92d376c65ba7
Ubbemir <ubbermir@example.com>
2025-03-12 21:37:04 +0100

    First minimum viable product

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,23 @@ import { useState, useEffect } from "react"
 import * as pyodide_worker_api from "./logic/pyodide_worker_api.mjs"
 
 function App() {
-  const [pyResult, ] = useState("")
+  const [pyResult, setPyResult] = useState("")
   const [pyReady, setpyReady] = useState(false)
+  const [inputCfg, setInputCfg] = useState("")
   
+  const inputChanged = async (e: any) => {
+    const input = e.target.value
+    setInputCfg(input)
+
+    if (pyReady) {
+      const { result, error } = await pyodide_worker_api.asyncRunCfgFormatting(input, true)
+      if (result)
+        setPyResult(result)
+      else
+        console.log(error)
+    }
+  }
+
   useEffect(() => {
     const load_formatters = async () => {
       const { result, error } = await pyodide_worker_api.asyncRunCfgFormatting("sv_cheats 1", true)
@@ -22,6 +36,7 @@ function App() {
     <>
       <p>{pyReady ? "Done" : "Initializing"}</p>
       <p>Result from Python:</p>
+      <textarea value={inputCfg} onInput={inputChanged}></textarea>
       <textarea value={pyResult} readOnly></textarea>
     </>
   )
